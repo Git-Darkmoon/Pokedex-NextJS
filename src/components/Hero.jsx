24710f60d13a285ab40e8964db660459f4ed6c0f
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-08 18:15:58 -0500

    feat: Hero component with the main logo.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import pikachuLogo from "@/assets/pikachu-hero.webp"
+
+function Hero() {
+  return (
+    <section className="h-[calc(100dvh-5rem)] flex justify-center items-center flex-col md:flex-row ">
+      <article>
+        <h1 className="text-3xl ">
+          Find all your favorite Pokemon in one search
+        </h1>
+      </article>
+      <Image
+        src={pikachuLogo}
+        alt="Pikachu Logo"
+        width={500}
+        height={500}
+        priority
+      />
+    </section>
+  )
+}
+export default Hero
