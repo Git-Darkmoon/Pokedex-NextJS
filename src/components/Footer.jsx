346bf51b4952103306116333f402c44244b8fc9a
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-10 22:24:39 -0500

    feat: Created the component and its structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,28 @@
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer className="w-full h-20 flex justify-left items-center px-5 md:px-10 lg:px-16 text-slate-600">
+      <p>
+        Built by{" "}
+        <a
+          className="text-orange-500 hover:text-orange-700 transition-colors"
+          href="https://www.linkedin.com/in/nicol%C3%A1s-mayorga-7z0/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nicolas Mayorga
+        </a>
+        . The source code is on{" "}
+        <a
+          className="text-orange-500 hover:text-orange-700 transition-colors"
+          href="https://github.com/Git-Darkmoon/Pokedex-NextJS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </footer>
+  )
 }
 export default Footer
