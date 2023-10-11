0cfcd1cc3c6563306244c586d579a2f6c6831f7e
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-10 22:26:15 -0500

    styles: responsive grid layout.

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -12,7 +12,7 @@ async function Pokemons() {
   const pokemons = await fetchPokemon()
 
   return (
-    <section>
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 mt-20">
       {pokemons.results.map((eachPokemon) => {
         return <PokemonCard key={eachPokemon.name} {...eachPokemon} />
       })}
