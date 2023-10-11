54c1602ab6d91e12b6dec80242dd34023933abcc
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-10 22:23:22 -0500

    feat: Footer added to the page layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar"
 import "./globals.css"
 import { Poppins } from "next/font/google"
+import Footer from "@/components/Footer"
 
 const ffPoppins = Poppins({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
       <body className={ffPoppins.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
