6ffeee5794e452c11272f8e9260703924b174b55
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-11 22:43:01 -0500

    feat: error component working properly.

---
+++ src/app/error.jsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Image from "next/image"
+import sadPikachu from "../../public/pikachu_sad.webp"
+import Link from "next/link"
+
+function ErrorPage() {
+  return (
+    <main className="min-h-[calc(100dvh-(5rem*2))] w-full flex flex-col md:flex-row justify-around items-center">
+      <article>
+        <h1>It looks like we have a Poke Error...</h1>
+        <h4>Maybe the pokemon / route searched its not valid.</h4>
+        <Link
+          className="bg-orange-400 hover:bg-orange-500 transition-colors px-6 py-3 rounded-md text-xl mt-3 inline-block"
+          href={"/"}
+        >
+          Go back
+        </Link>
+      </article>
+      <Image
+        src={sadPikachu}
+        alt="sad pikachu"
+        width={400}
+        height={400}
+        className="md:w-[30rem] md:h-[25rem]"
+      />
+    </main>
+  )
+}
+export default ErrorPage
