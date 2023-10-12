0e926c36cfe984e0187cbb9412bc4427537a5c86
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-11 20:15:43 -0500

    feat: Dynamic search for a specific pokemon.

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,23 +1,38 @@
+"use client"
+
+import { useState } from "react"
 import Link from "next/link"
 
 function SearchForm() {
+  const [searchValue, setSearchValue] = useState("")
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value)
+  }
+
   return (
-    <form className="flex flex-col bg-yellow-100 shadow-lg my-4 p-5 rounded-md border-solid border-2 border-orange-200 md:w-[calc(100%/2)] mx-auto">
+    <section className="flex flex-col bg-yellow-100 shadow-lg my-4 p-5 rounded-md border-solid border-2 border-orange-200 md:w-[calc(100%/2)] mx-auto">
       <label htmlFor="pokeSearch" className="">
-        Looking for some specific pokemon ?
+        Looking for some specific Pokemon?
       </label>
       <input
+        className="rounded-sm my-3 p-2 focus:outline-8 focus:outline-solid focus:outline-orange-300"
         type="search"
-        placeholder="i.e. pikachu"
+        placeholder="i.e. Pikachu"
         autoComplete="off"
         name="search"
         id="pokeSearch"
-        className="rounded-sm my-3 p-2 focus:outline-8 focus:outline-solid focus:outline-orange-300"
+        value={searchValue}
+        onChange={handleInputChange}
       />
-      <Link className="bg-orange-400 px-4 py-2 rounded-md hover:bg-orange-500 transition-colors">
+      <Link
+        href={`/${searchValue}`}
+        className="bg-orange-400 px-4 py-2 rounded-md hover:bg-orange-500 transition-colors text-center"
+      >
         Search
       </Link>
-    </form>
+    </section>
   )
 }
+
 export default SearchForm
