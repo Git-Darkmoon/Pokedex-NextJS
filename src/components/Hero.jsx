450b19ef6ce5d2b0599db48b0c81b3e08ed12edb
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-11 21:16:55 -0500

    styles: edited base structure of  hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
 import Image from "next/image"
-import pikachuLogo from "@/assets/pikachu-hero.webp"
+import pikachuLogo from "../../public/pikachu-hero.webp"
 
 function Hero() {
   return (
     <section className="h-[calc(100dvh-5rem)] flex justify-center items-center flex-col md:flex-row ">
       <article>
-        <h1 className="text-3xl ">
+        <h1 className="text-3xl max-w-[35ch]">
           Find all your favorite Pokemon in one search
         </h1>
+        <br />
+        <h2 className="text-2xl">What are you waiting for? </h2>
+        <a
+          href="#query"
+          className="bg-orange-400 hover:bg-orange-500 transition-colors px-6 py-3 rounded-md text-xl mt-3 inline-block "
+        >
+          Get Started
+        </a>
       </article>
       <Image
         src={pikachuLogo}
