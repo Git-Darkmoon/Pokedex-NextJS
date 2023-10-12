643479f29e226aa86c29d41165823758e8d41685
Nicolas Mayorga <namayorgav@gmail.com>
2023-10-11 20:04:04 -0500

    feat: Dynamic RSC for details of single pokemon.

---
+++ src/app/[pokemon]/page.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image"
+
+async function fetchPokemon(param) {
+  const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${param}`)
+  const data = await resp.json()
+  return data
+}
+
+async function PokemonPage({ params }) {
+  const { pokemon } = params
+  const pokemonData = await fetchPokemon(pokemon)
+
+  const pokemonName = pokemonData?.name || "Pokemon Not Available"
+  const pokeAvatar = pokemonData?.sprites?.other?.dream_world?.front_default
+  const { types } = pokemonData
+
+  return (
+    <main className="h-[calc(100dvh-(5rem*2))] flex flex-col md:flex-row justify-between items-center px-5 md:px-10 lg:px-16">
+      <picture>
+        <Image src={pokeAvatar} alt={pokemonName} width={400} height={400} />
+      </picture>
+      <article className="flex flex-col w-[50%]">
+        <h1 className="capitalize text-7xl">{pokemonName}</h1>
+        <div className="flex">
+          {"("}
+          {types.map((type) => {
+            return <h5 key={crypto.randomUUID()}>{type.type.name}</h5>
+          })}
+          {")"}
+        </div>
+
+        <br />
+        <h2>Statistics:</h2>
+        <br />
+        {pokemonData?.stats.map((stat) => {
+          return (
+            <div key={crypto.randomUUID()}>
+              <div className="flex justify-between">
+                <h3>{stat.stat.name}</h3>
+                <h3>{stat.base_stat}</h3>
+              </div>
+            </div>
+          )
+        })}
+      </article>
+    </main>
+  )
+}
+export default PokemonPage
